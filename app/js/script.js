50669cbac97c58e091f30bc3478455d6a34ab15a
romelmahmud <romelmahmud@outlook.com>
2021-01-27 17:02:23 +0600

    ovrlay animation create

--- app/js/script.js
+++ app/js/script.js
@@ -1,7 +1,17 @@
 const burgerMenu = document.querySelector('#burger__menu');
+const header = document.querySelector('.header');
+
+const overlay = document.querySelector('.overlay')
 
 burgerMenu.addEventListener('click', function(){
-   if (burgerMenu.classList.contains('open')) {
-      burgerMenu.classList.remove('open');
-   } else { burgerMenu.classList.add('open'); }
+   if (header.classList.contains('open')) {
+      header.classList.remove('open');
+      overlay.classList.remove('fade-in');
+      overlay.classList.add('fade-out');
+      
+   } else {
+      header.classList.add('open');
+      overlay.classList.remove('fade-out');
+      overlay.classList.add('fade-in');
+      }
 });
