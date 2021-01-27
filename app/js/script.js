da2f79bc08e033c5a8502028c16710f3553a6bac
romelmahmud <romelmahmud@outlook.com>
2021-01-27 18:54:42 +0600

    basic dropdown menu complete

--- app/js/script.js
+++ app/js/script.js
@@ -2,16 +2,21 @@ const burgerMenu = document.querySelector('#burger__menu');
 const header = document.querySelector('.header');
 
 const overlay = document.querySelector('.overlay')
+const menu = document.querySelector('.header__menu')
 
 burgerMenu.addEventListener('click', function(){
    if (header.classList.contains('open')) {
       header.classList.remove('open');
       overlay.classList.remove('fade-in');
       overlay.classList.add('fade-out');
+      menu.classList.remove('fade-in');
+      menu.classList.add('fade-out');
       
    } else {
       header.classList.add('open');
       overlay.classList.remove('fade-out');
       overlay.classList.add('fade-in');
+      menu.classList.remove('fade-out');
+      menu.classList.add('fade-in');
       }
 });
